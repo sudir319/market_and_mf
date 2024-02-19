cbbe59e98fc6d92cdce398bc7fd04ae2aa3cf0be
Sudheer Chigurupati <sudir.319@gmail.com>
2024-02-19 09:08:02 +0530

    Latest build

--- src/components/BuildInfo.js
+++ src/components/BuildInfo.js
@@ -1,10 +1,22 @@
 import React from "react";
+import marketDataSummary from "../json_data/market_data_summary.json";
 
 export default function VersionInfo() {
-  const currentDate = new Date();
+  const candle_data = marketDataSummary["NSE"]["candle_data"];
+  const stockdata =
+    candle_data[Object.keys(marketDataSummary["NSE"]["candle_data"])[0]];
+  const lastCandle = stockdata[stockdata.length - 1];
+  const lastCandleDate = lastCandle["date"];
+  const currentDate = new Date(Date.parse(lastCandleDate));
   const year = currentDate.getFullYear();
-  const month = currentDate.getMonth() + 1;
-  const day = currentDate.getDate();
+  const month =
+    currentDate.getMonth() < 9
+      ? "0" + (currentDate.getMonth() + 1)
+      : currentDate.getMonth() + 1;
+  const day =
+    currentDate.getDate() < 10
+      ? "0" + currentDate.getDate()
+      : currentDate.getDate();
   const version = `${year}.${month}.${day}`;
   return <div align="center">This is build on {version}</div>;
 }
